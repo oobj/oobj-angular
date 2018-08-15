fa8ac15f192368916226f0907aeb380669625a00
Raphael Ferreira <raphaelbsferreira@gmail.com>
2018-08-14 23:59:05 -0300

    Atulizando docs

--- build-docs.sh
+++ build-docs.sh
@@ -1,4 +1,4 @@
-npx ng build --prod --aot=false --build-optimizer=false --output-path docs  --base-href /oobj-angular --deploy-url '/'
+npx ng build --prod --aot=false --build-optimizer=false --output-path docs  --base-href /oobj-angular --deploy-url '/oobj-angular'
 mv docs/index.html docs/404.html
 cd projects/oobj/angular
 npm run doc
